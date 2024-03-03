73d38532f97a337419007deaf5109064ca89a724
jaemin <jjm7655@gmail.com>
2024-03-03 18:16:25 +0900

    init

---
+++ src/test/java/org/example/webflux1/Webflux1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.webflux1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webflux1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
