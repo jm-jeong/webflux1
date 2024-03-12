23196af5f0bec529a210782d91d064d587fd41cb
jaemin <jjm7655@gmail.com>
2024-03-12 18:35:29 +0900

    blockhound

--- src/main/java/org/example/webflux1/Webflux1Application.java
+++ src/main/java/org/example/webflux1/Webflux1Application.java
@@ -2,11 +2,14 @@ package org.example.webflux1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class Webflux1Application {
 
     public static void main(String[] args) {
+        BlockHound.install();
+
         SpringApplication.run(Webflux1Application.class, args);
     }
 
